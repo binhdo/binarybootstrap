3f49501999b3afecc6b223f264c60238cb8152b9
binhdo <m@binaryhideout.com>
2013-03-09 20:19:53 +0100

    load bootstrap scripts & styles

--- functions.php
+++ functions.php
@@ -144,18 +144,21 @@ add_action( 'widgets_init', 'binarybootstrap_widgets_init' );
  * Enqueue scripts and styles
  */
 function binarybootstrap_scripts_styles() {
+	// Load Bootstrap stylesheet
+	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', false, null );
+	
+	// Load style.css
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
-
-	wp_enqueue_script( 'navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
-	wp_enqueue_script( 'skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	
+	// Load Bootstrap javascripts
+	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array( 'jquery' ), null, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 
 	if ( is_singular() && wp_attachment_is_image() ) {
-		wp_enqueue_script( 'keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
+		wp_enqueue_script( 'keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), null, true );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'binarybootstrap_scripts_styles' );
