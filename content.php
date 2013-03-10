d2ada883e7d67c3621ea240a9d0233bb2dbd4f97
binhdo <m@binaryhideout.com>
2013-03-10 14:07:48 +0100

    remove dispensable functions / cleanup

--- content.php
+++ content.php
@@ -28,33 +28,6 @@
 	<?php endif; ?>
 
 	<footer class="entry-meta">
-		<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$categories_list = get_the_category_list( __( ', ', 'binarybootstrap' ) );
-				if ( $categories_list && binarybootstrap_categorized_blog() ) :
-			?>
-			<span class="cat-links">
-				<?php printf( __( 'Posted in %1$s', 'binarybootstrap' ), $categories_list ); ?>
-			</span>
-			<?php endif; // End if categories ?>
-
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$tags_list = get_the_tag_list( '', __( ', ', 'binarybootstrap' ) );
-				if ( $tags_list ) :
-			?>
-			<span class="sep"> | </span>
-			<span class="tags-links">
-				<?php printf( __( 'Tagged %1$s', 'binarybootstrap' ), $tags_list ); ?>
-			</span>
-			<?php endif; // End if $tags_list ?>
-		<?php endif; // End if 'post' == get_post_type() ?>
-
-		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-		<span class="sep"> | </span>
-		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'binarybootstrap' ), __( '1 Comment', 'binarybootstrap' ), __( '% Comments', 'binarybootstrap' ) ); ?></span>
-		<?php endif; ?>
 
 		<?php edit_post_link( __( 'Edit', 'binarybootstrap' ), '<span class="sep"> | </span><span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-meta -->
--- inc/binarybootstrap-template-tags.php
+++ inc/binarybootstrap-template-tags.php
@@ -68,7 +68,6 @@ function binarybootstrap_nav_menu( $location, $class, $brand_text = null, $brand
 	}
 }
 
-if ( ! function_exists( 'binarybootstrap_content_nav' ) ) :
 /**
  * Display navigation to next/previous pages when applicable
  *
@@ -116,9 +115,7 @@ function binarybootstrap_content_nav( $nav_id ) {
 	</nav><!-- #<?php echo esc_html( $nav_id ); ?> -->
 	<?php
 }
-endif; // binarybootstrap_content_nav
 
-if ( ! function_exists( 'binarybootstrap_posted_on' ) ) :
 /**
  * Prints HTML with meta information for the current post-date/time and author.
  *
@@ -135,42 +132,3 @@ function binarybootstrap_posted_on() {
 		get_the_author()
 	);
 }
-endif;
-/**
- * Returns true if a blog has more than 1 category
- *
- * @since Binary Bootstrap 1.0
- */
-function binarybootstrap_categorized_blog() {
-	if ( false === ( $all_the_cool_cats = get_transient( 'all_the_cool_cats' ) ) ) {
-		// Create an array of all the categories that are attached to posts
-		$all_the_cool_cats = get_categories( array(
-			'hide_empty' => 1,
-		) );
-
-		// Count the number of categories that are attached to the posts
-		$all_the_cool_cats = count( $all_the_cool_cats );
-
-		set_transient( 'all_the_cool_cats', $all_the_cool_cats );
-	}
-
-	if ( '1' != $all_the_cool_cats ) {
-		// This blog has more than 1 category so binarybootstrap_categorized_blog should return true
-		return true;
-	} else {
-		// This blog has only 1 category so binarybootstrap_categorized_blog should return false
-		return false;
-	}
-}
-
-/**
- * Flush out the transients used in binarybootstrap_categorized_blog
- *
- * @since Binary Bootstrap 1.0
- */
-function binarybootstrap_category_transient_flusher() {
-	// Like, beat it. Dig?
-	delete_transient( 'all_the_cool_cats' );
-}
-add_action( 'edit_category', 'binarybootstrap_category_transient_flusher' );
-add_action( 'save_post', 'binarybootstrap_category_transient_flusher' );
