789eb764fadfa8ee48a40a60d87fa3cfeaeb2341
binhdo <m@binaryhideout.com>
2013-03-11 21:16:27 +0100

    remove unused filter, wording

--- content.php
+++ content.php
@@ -22,7 +22,7 @@
 	</div><!-- .entry-summary -->
 	<?php else : ?>
 	<div class="entry-content">
-		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'binarybootstrap' ) ); ?>
+		<?php the_content( __( 'Read more <span class="meta-nav">&rarr;</span>', 'binarybootstrap' ) ); ?>
 		<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'binarybootstrap' ), 'after' => '</div>' ) ); ?>
 	</div><!-- .entry-content -->
 	<?php endif; ?>
--- inc/binarybootstrap-filters.php
+++ inc/binarybootstrap-filters.php
@@ -9,17 +9,6 @@
  */
 
 /**
- * Get our wp_nav_menu() fallback, wp_page_menu(), to show a home link.
- *
- * @since Binary Bootstrap 1.0
- */
-function binarybootstrap_page_menu_args( $args ) {
-	$args['show_home'] = true;
-	return $args;
-}
-add_filter( 'wp_page_menu_args', 'binarybootstrap_page_menu_args' );
-
-/**
  * Adds custom classes to the array of body classes.
  *
  * @since Binary Bootstrap 1.0
