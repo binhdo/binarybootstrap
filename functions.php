2a5d13ba7cfcb3e0789d2a3156d5646372b3bd8a
binhdo <m@binaryhideout.com>
2013-03-12 21:58:29 +0100

    don't load useless keyboard-navigation script

--- functions.php
+++ functions.php
@@ -1,52 +1,52 @@
 <?php
+
 /**
  * Binary Bootstrap functions and definitions
  *
  * @package Binary Bootstrap
  * @since Binary Bootstrap 1.0
  */
-
 /**
  * Custom template tags for this theme.
  */
-require_once locate_template('/inc/binarybootstrap-template-tags.php');
+require_once locate_template( '/inc/binarybootstrap-template-tags.php' );
 
 /**
  * Custom functions that act independently of the theme templates
-*/
-require_once locate_template('/inc/binarybootstrap-filters.php');
+ */
+require_once locate_template( '/inc/binarybootstrap-filters.php' );
 
 /**
  * Custom Comment Walker
-*/
-require_once locate_template('/inc/binarybootstrap-walker-comment.php');
+ */
+require_once locate_template( '/inc/binarybootstrap-walker-comment.php' );
 
 /**
  * Custom Menu Walker
-*/
-require_once locate_template('/inc/binarybootstrap-walker-menu.php');
+ */
+require_once locate_template( '/inc/binarybootstrap-walker-menu.php' );
 
 /**
  * Customizer additions
-*/
-require_once locate_template('/inc/binarybootstrap-customizer.php');
+ */
+require_once locate_template( '/inc/binarybootstrap-customizer.php' );
 
 /**
  * Implement the Custom Header feature
-*/
-require_once locate_template('/inc/binarybootstrap-custom-header.php');
+ */
+require_once locate_template( '/inc/binarybootstrap-custom-header.php' );
 
 /*
  * Load Jetpack compatibility file.
-*/
-require_once locate_template('/inc/jetpack.php');
+ */
+require_once locate_template( '/inc/jetpack.php' );
 
 /**
  * Set the content width based on the theme's design and stylesheet.
  *
  * @since Binary Bootstrap 1.0
-*/
-if ( ! isset( $content_width ) )
+ */
+if ( !isset( $content_width ) )
 	$content_width = 640; /* pixels */
 
 /**
@@ -70,27 +70,28 @@ function binarybootstrap_setup() {
 
 	/**
 	 * Add default posts and comments RSS feed links to head
-	*/
+	 */
 	add_theme_support( 'automatic-feed-links' );
 
 	/**
 	 * Enable support for Post Thumbnails
-	*/
+	 */
 	add_theme_support( 'post-thumbnails' );
 
 	/**
 	 * This theme uses wp_nav_menu() in two locations.
-	*/
+	 */
 	register_nav_menus( array(
-	'top_nav' => __( 'Top Navbar', 'binarybootstrap' ),
-	'primary' => __( 'Primary Menu', 'binarybootstrap' ),
+		'top_nav' => __( 'Top Navbar', 'binarybootstrap' ),
+		'primary' => __( 'Primary Menu', 'binarybootstrap' ),
 	) );
 
 	/**
 	 * Enable support for Post Formats
-	*/
-	add_theme_support( 'post-formats', array( 'aside', 'image', 'video', 'quote', 'link' ) );
+	 */
+	add_theme_support( 'post-formats', array('aside', 'image', 'video', 'quote', 'link') );
 }
+
 add_action( 'after_setup_theme', 'binarybootstrap_setup' );
 
 /**
@@ -104,11 +105,11 @@ add_action( 'after_setup_theme', 'binarybootstrap_setup' );
  * @todo Remove the 3.3 support when WordPress 3.6 is released.
  *
  * Hooks into the after_setup_theme action.
-*/
+ */
 function binarybootstrap_register_custom_background() {
 	$args = array(
-			'default-color' => 'ffffff',
-			'default-image' => '',
+		'default-color' => 'ffffff',
+		'default-image' => '',
 	);
 
 	$args = apply_filters( 'binarybootstrap_custom_background_args', $args );
@@ -117,44 +118,46 @@ function binarybootstrap_register_custom_background() {
 		add_theme_support( 'custom-background', $args );
 	} else {
 		define( 'BACKGROUND_COLOR', $args['default-color'] );
-		if ( ! empty( $args['default-image'] ) )
+		if ( !empty( $args['default-image'] ) )
 			define( 'BACKGROUND_IMAGE', $args['default-image'] );
 		add_custom_background();
 	}
 }
+
 add_action( 'after_setup_theme', 'binarybootstrap_register_custom_background' );
 
 /**
  * Register widgetized area and update sidebar with default widgets
  *
  * @since Binary Bootstrap 1.0
-*/
+ */
 function binarybootstrap_widgets_init() {
 	register_sidebar( array(
-	'name'          => __( 'Sidebar', 'binarybootstrap' ),
-	'id'            => 'sidebar-1',
-	'description'   => __( 'The Sidebar containing the main widget areas.', 'binarybootstrap' ),
-	'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-	'after_widget'  => '</aside>',
-	'before_title'  => '<h3 class="widget-title">',
-	'after_title'   => '</h3>',
+		'name' => __( 'Sidebar', 'binarybootstrap' ),
+		'id' => 'sidebar-1',
+		'description' => __( 'The Sidebar containing the main widget areas.', 'binarybootstrap' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
 	) );
 
 	register_sidebar( array(
-	'name'			=> __( 'Footer Sidebar', 'binarybootstrap' ),
-	'id'			=> 'sidebar-2',
-	'description'   => __( 'The Sidebar containing the footer widget area.', 'binarybootstrap' ),
-	'before_widget' => '<aside id="%1$s" class="' . binarybootstrap_footer_widgets_class() . ' widget %2$s">',
-	'after_widget'  => '</aside>',
-	'before_title'  => '<h3 class="widget-title">',
-	'after_title'   => '</h3>',
+		'name' => __( 'Footer Sidebar', 'binarybootstrap' ),
+		'id' => 'sidebar-2',
+		'description' => __( 'The Sidebar containing the footer widget area.', 'binarybootstrap' ),
+		'before_widget' => '<aside id="%1$s" class="' . binarybootstrap_footer_widgets_class() . ' widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
 	) );
 }
+
 add_action( 'widgets_init', 'binarybootstrap_widgets_init' );
 
 /**
  * Enqueue scripts and styles
-*/
+ */
 function binarybootstrap_scripts_styles() {
 	// Load Bootstrap stylesheet
 	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', false, null );
@@ -163,14 +166,11 @@ function binarybootstrap_scripts_styles() {
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
 
 	// Load Bootstrap javascripts
-	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array( 'jquery' ), null, true );
+	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), null, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
-
-	if ( is_singular() && wp_attachment_is_image() ) {
-		wp_enqueue_script( 'keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), null, true );
-	}
 }
+
 add_action( 'wp_enqueue_scripts', 'binarybootstrap_scripts_styles' );
