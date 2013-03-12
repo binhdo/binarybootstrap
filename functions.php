04ce8e1743074ccf960357a925ad1f6a9e51b725
binhdo <m@binaryhideout.com>
2013-03-12 23:09:41 +0100

    remove custom background feature

--- functions.php
+++ functions.php
@@ -95,38 +95,6 @@ function binarybootstrap_setup() {
 add_action( 'after_setup_theme', 'binarybootstrap_setup' );
 
 /**
- * Setup the WordPress core custom background feature.
- *
- * Use add_theme_support to register support for WordPress 3.4+
- * as well as provide backward compatibility for WordPress 3.3
- * using feature detection of wp_get_theme() which was introduced
- * in WordPress 3.4.
- *
- * @todo Remove the 3.3 support when WordPress 3.6 is released.
- *
- * Hooks into the after_setup_theme action.
- */
-function binarybootstrap_register_custom_background() {
-	$args = array(
-		'default-color' => 'ffffff',
-		'default-image' => '',
-	);
-
-	$args = apply_filters( 'binarybootstrap_custom_background_args', $args );
-
-	if ( function_exists( 'wp_get_theme' ) ) {
-		add_theme_support( 'custom-background', $args );
-	} else {
-		define( 'BACKGROUND_COLOR', $args['default-color'] );
-		if ( !empty( $args['default-image'] ) )
-			define( 'BACKGROUND_IMAGE', $args['default-image'] );
-		add_custom_background();
-	}
-}
-
-add_action( 'after_setup_theme', 'binarybootstrap_register_custom_background' );
-
-/**
  * Register widgetized area and update sidebar with default widgets
  *
  * @since Binary Bootstrap 1.0
