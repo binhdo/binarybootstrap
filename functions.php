bbaee35889417b1d84b19e86ce44da83c8d69f26
binhdo <m@binaryhideout.com>
2013-03-13 19:42:09 +0100

    nice-search redirect, modify search form

--- functions.php
+++ functions.php
@@ -150,3 +150,24 @@ function binarybootstrap_content_width() {
 }
 
 add_action( 'template_redirect', 'binarybootstrap_content_width' );
+
+/**
+ * Redirects search results from /?s=query to /search/query/, converts %20 to +
+ *
+ * @link http://txfx.net/wordpress-plugins/nice-search/
+ */
+function binarybootstrap_nice_search_redirect() {
+	global $wp_rewrite;
+	if ( !isset( $wp_rewrite ) || !is_object( $wp_rewrite ) || !$wp_rewrite->using_permalinks() ) {
+		return;
+	}
+
+	$search_base = $wp_rewrite->search_base;
+	if ( is_search() && !is_admin() && strpos( $_SERVER['REQUEST_URI'], "/{$search_base}/" ) === false ) {
+		wp_redirect( home_url( "/{$search_base}/" . urlencode( get_query_var( 's' ) ) ) );
+		exit();
+	}
+}
+
+add_action( 'template_redirect', 'binarybootstrap_nice_search_redirect' );
+
--- searchform.php
+++ searchform.php
@@ -8,6 +8,6 @@
 ?>
 	<form method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
 		<label for="s" class="assistive-text"><?php _ex( 'Search', 'assistive text', 'binarybootstrap' ); ?></label>
-		<input type="search" class="field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'binarybootstrap' ); ?>" />
-		<input type="submit" class="submit" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'binarybootstrap' ); ?>" />
+		<input type="search" class="field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'binarybootstrap' ); ?>">
+		<input type="submit" class="btn submit" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'binarybootstrap' ); ?>" />
 	</form>
