ea7052aa399cb14bb9657d7df9ae826404a8f666
binhdo <m@binaryhideout.com>
2013-06-29 22:04:57 +0200

    simplified archive titles

--- archive.php
+++ archive.php
@@ -18,52 +18,60 @@ get_header(); ?>
 			<header class="page-header">
 				<h1 class="page-title">
 					<?php
-						if ( is_category() ) {
-							printf( __( 'Category Archives: %s', 'binarybootstrap' ), '<span>' . single_cat_title( '', false ) . '</span>' );
+						if ( is_category() ) :
+							single_cat_title();
 
-						} elseif ( is_tag() ) {
-							printf( __( 'Tag Archives: %s', 'binarybootstrap' ), '<span>' . single_tag_title( '', false ) . '</span>' );
+						elseif ( is_tag() ) :
+							single_tag_title();
 
-						} elseif ( is_author() ) {
+						elseif ( is_author() ) :
 							/* Queue the first post, that way we know
 							 * what author we're dealing with (if that is the case).
 							*/
 							the_post();
-							printf( __( 'Author Archives: %s', 'binarybootstrap' ), '<span class="vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( "ID" ) ) ) . '" title="' . esc_attr( get_the_author() ) . '" rel="me">' . get_the_author() . '</a></span>' );
+							printf( __( 'Author: %s', 'binarybootstrap' ), '<span class="vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '" title="' . esc_attr( get_the_author() ) . '" rel="me">' . get_the_author() . '</a></span>' );
 							/* Since we called the_post() above, we need to
 							 * rewind the loop back to the beginning that way
 							 * we can run the loop properly, in full.
 							 */
 							rewind_posts();
 
-						} elseif ( is_day() ) {
-							printf( __( 'Daily Archives: %s', 'binarybootstrap' ), '<span>' . get_the_date() . '</span>' );
+						elseif ( is_day() ) :
+							printf( __( 'Day: %s', 'binarybootstrap' ), '<span>' . get_the_date() . '</span>' );
 
-						} elseif ( is_month() ) {
-							printf( __( 'Monthly Archives: %s', 'binarybootstrap' ), '<span>' . get_the_date( 'F Y' ) . '</span>' );
+						elseif ( is_month() ) :
+							printf( __( 'Month: %s', 'binarybootstrap' ), '<span>' . get_the_date( 'F Y' ) . '</span>' );
 
-						} elseif ( is_year() ) {
-							printf( __( 'Yearly Archives: %s', 'binarybootstrap' ), '<span>' . get_the_date( 'Y' ) . '</span>' );
+						elseif ( is_year() ) :
+							printf( __( 'Year: %s', 'binarybootstrap' ), '<span>' . get_the_date( 'Y' ) . '</span>' );
 
-						} else {
+						elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
+							_e( 'Asides', 'binarybootstrap' );
+
+						elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
+							_e( 'Images', 'binarybootstrap');
+
+						elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
+							_e( 'Videos', 'binarybootstrap' );
+
+						elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
+							_e( 'Quotes', 'binarybootstrap' );
+
+						elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
+							_e( 'Links', 'binarybootstrap' );
+
+						else :
 							_e( 'Archives', 'binarybootstrap' );
 
-						}
+						endif;
 					?>
 				</h1>
 				<?php
-					if ( is_category() ) {
-						// show an optional category description
-						$category_description = category_description();
-						if ( ! empty( $category_description ) )
-							echo apply_filters( 'category_archive_meta', '<div class="taxonomy-description">' . $category_description . '</div>' );
-
-					} elseif ( is_tag() ) {
-						// show an optional tag description
-						$tag_description = tag_description();
-						if ( ! empty( $tag_description ) )
-							echo apply_filters( 'tag_archive_meta', '<div class="taxonomy-description">' . $tag_description . '</div>' );
-					}
+					// Show an optional term description.
+					$term_description = term_description();
+					if ( ! empty( $term_description ) ) :
+						printf( '<div class="taxonomy-description">%s</div>', $term_description );
+					endif;
 				?>
 			</header><!-- .page-header -->
 
