247e696ca0386d98df261f4ded2dafb6470f1152
binhdo <m@binaryhideout.com>
2013-08-09 19:01:57 +0200

    make sure search-form actually works

--- searchform.php
+++ searchform.php
@@ -7,14 +7,14 @@
  * @since Binary Bootstrap 1.0
  */
 ?>
-<form role="search" method="get" class="search-form" action="<?php esc_url( home_url( '/' ) ); ?>">
+<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<div class="input-group">
 		<label>
 			<span class="screen-reader-text"><?php echo _x( 'Search for:', 'binarybootstrap' ); ?></span>
 		</label>
-		<input type="search" class="search-field form-control" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'binarybootstrap' ); ?>" value="<?php get_search_query(); ?>" name="s" title="<?php echo _x( 'Search for:', 'binarybootstrap' ); ?>">
+		<input type="search" class="search-field form-control" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'binarybootstrap' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo _x( 'Search for:', 'binarybootstrap' ); ?>">
 		<span class="input-group-btn">
-		<input type="submit" class="search-submit btn btn-default" value="<?php echo esc_attr_x( 'Search', 'binarybootstrap' ); ?>">
+			<button type="submit" class="search-submit btn btn-default"><?php echo esc_attr_x( 'Search', 'binarybootstrap' ); ?></button>
 		</span>
 	</div>
 </form>
